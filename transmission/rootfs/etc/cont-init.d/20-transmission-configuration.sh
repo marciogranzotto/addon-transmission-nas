ab755e40134fda66112c2a4fb632bc4678ef2e01
Marcio Granzotto <oscensores@gmail.com>
2020-06-16 15:06:00 +0000

    Disable incomplete-dir and default to mounted SMB drive for download dir

--- transmission/rootfs/etc/cont-init.d/20-transmission-configuration.sh
+++ transmission/rootfs/etc/cont-init.d/20-transmission-configuration.sh
@@ -5,6 +5,7 @@ declare CONFIG
 declare authentication_required
 declare username
 declare password
+declare download_dir
 
 if ! bashio::fs.directory_exists '/data/transmission'; then
   mkdir '/data/transmission'
@@ -17,12 +18,24 @@ fi
 CONFIG=$(</data/transmission/settings.json)
 
 # Defaults
-CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir\"=\"/share/incomplete\"")
-CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir-enabled\"=true")
-CONFIG=$(bashio::jq "${CONFIG}" ".\"download-dir\"=\"/share/downloads\"")
+CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir-enabled\"=false")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-whitelist-enabled\"=false")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-host-whitelist-enabled\"=false")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"bind-address-ipv4\"=\"0.0.0.0\"")
+
+download_dir="/share/downloads"
+if bashio::config.has_value 'networkdisks'; then
+    MOREDISKS=$(bashio::config 'networkdisks')
+    for disk in $MOREDISKS 
+    do
+      download_dir=$disk
+      bashio::log.info "Setting default download folder to: ${download_dir}"
+      break 
+    done
+fi
+
+CONFIG=$(bashio::jq "${CONFIG}" ".\"download-dir\"=\"${download_dir}\"")
+
 
 authentication_required=$(bashio::config 'authentication_required')
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-authentication-required\"=${authentication_required}")
